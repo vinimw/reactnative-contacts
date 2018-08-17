f8fa2ce23f2fc46e502630948c4c6b818b3efa25
Vinicius Weber <vinicius.weber@VINICIUSWEBERMAC.local>
2018-08-17 11:25:53 -0300

    show image profile

--- src/pages/PeopleDetailPage.js
+++ src/pages/PeopleDetailPage.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
 
 export default class PeopleDetailPage extends React.Component {
 	render() {
+		const { people } = this.props.navigation.state.params;
+
 		return (
 			<View>
+				<View style={styles.frame}>
+					<Image source={{ uri: people.picture.large}} style={styles.avatar} />
+				</View>
 				<Text>I arrived here Detail</Text>
 			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	avatar: {
+		aspectRatio: 1,
+		borderRadius: 50
+	},
+	frame: {
+		padding: 10
+	}
+});
